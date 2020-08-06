ed5fdb43334fee8ebfb0ce9840f32865f9bd91e0
cesarramos95 <cesarramos@gmail.com>
2020-08-06 15:03:51 -0300

    Add Nunjucks setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const proffys = [
     {
         nome: "Diego Fernandes",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4" alt="Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "89787878787",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
@@ -12,7 +12,7 @@ const proffys = [
     },
     {
         nome: "Daniela Evangelista",
-        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4" alt="Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
         whatsapp: "89787878787",
         bio: "Entusiasta das melhores tecnologias de química avançada.<br><br>Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
         subject: "Química",
@@ -24,22 +24,30 @@ const proffys = [
 ]
 
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html") // Concatenation to find the home page file
+    return res.render("index.html") // Concatenation to find the home page file
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    return res.render("study.html")
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    return res.render("give-classes.html")
 }
 
 const express = require('express')
 const server = express()
 
+// Configure nunjucks
+const nunjucks = require ('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
+// Configure statics files(CSS, scripts, images)
 server.use(express.static("public")) // Setting to read the "public folder"
+// Application routes
 .get("/", pageLanding)
 .get("/study", pageStudy)
 .get("/give-classes", pageGiveClasses)
